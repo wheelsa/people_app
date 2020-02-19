1867376bb90b9395741abc373def5900c84860a0
Seth Wheeler <wheeler.seth@gmail.com>
2020-02-19 16:58:20 -0700

    added static pages and index bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get '/about', to: 'static_pages#about'
   resources :people
   resources :teams
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
